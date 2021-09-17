f6bb267c8d3f4154d1499e526eeec56d6ed121ed
Josue Kula <koolmx9@gmail.com>
2021-09-16 22:45:04 -0500

    modify models

--- messaging/models.py
+++ messaging/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 # Create your models here.
+# Example models from django
 class Publisher(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
@@ -10,26 +11,3 @@ class Publisher(models.Model):
     country = models.CharField(max_length=50)
     website = models.URLField(max_length=200)
 
-    class Meta:
-        ordering = ["-name"]
-
-    def __str__(self):
-        return self.name
-
-
-class Author(models.Model):
-    salutation = models.CharField(max_length=10)
-    name = models.CharField(max_length=200)
-    email = models.EmailField()
-    # headshot = models.ImageField(upload_to='author_headshots')
-
-    def __str__(self):
-        return self.name
-
-
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    authors = models.ManyToManyField('Author')
-    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
-    publication_date = models.DateField()
-    
